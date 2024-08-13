30984a0ce9b93f2ba6f5136e8573234fc24b63bd
Tran Thuan <dtthuan.1211@gmail.com>
2024-08-13 16:29:22 +0700

    try this
    try header

---
+++ header.js
@@ -0,0 +1,8 @@
+// header.js
+document.addEventListener("DOMContentLoaded", function() {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header-container').innerHTML = data;
+        });
+});
