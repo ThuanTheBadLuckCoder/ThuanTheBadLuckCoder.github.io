dde7b0a996dafa5dde70cfa16990918e69e27dc2
Tran Thuan <dtthuan.1211@gmail.com>
2024-08-14 17:45:07 +0700

    turn back button
    test

--- header.js
+++ header.js
@@ -15,3 +15,38 @@ document.addEventListener("DOMContentLoaded", function() {
             document.getElementById('left-navbar').innerHTML = data;
         });
 });
+
+
+// Get the button, popup div, and overlay
+const button = document.getElementById("popupButton");
+const popup = document.getElementById("popupDiv");
+const overlay = document.getElementById("overlay");
+const close = document.querySelector(".popup .close");
+
+// Show the popup and overlay when the button is clicked
+button.addEventListener("click", function() {
+    popup.style.display = "block";
+    overlay.style.display = "block";
+});
+
+// Hide the popup and overlay when the close button is clicked
+close.addEventListener("click", function() {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+});
+
+// Hide the popup and overlay when clicking outside the popup
+overlay.addEventListener("click", function() {
+    popup.style.display = "none";
+    overlay.style.display = "none";
+});
+
+function goBack() {
+    if (document.referrer) {
+        // If there is a previous page, navigate to it
+        window.history.back();
+    } else {
+        // If no referrer is available, navigate to a default page (optional)
+        window.location.href = 'https://thuanthebadluckcoder.github.io/';
+    }
+}
